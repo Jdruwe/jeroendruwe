5710497de8046452fae9b84c2eabda15b980335d
Jeroen Druwe <jeroen.druwe@weareida.digital>
2026-01-04 22:14:14 +0100

    working on og image

---
+++ src/consts.ts
@@ -0,0 +1,11 @@
+// todo: update
+const SITE = {
+  title: 'Jeroen Druwé',
+  description:
+    'astro-erudite is a opinionated, unstyled blogging template—built with Astro, Tailwind, and shadcn/ui.',
+  href: 'https://jeroendruwe.be',
+  author: 'Jeroen Druwé',
+  locale: 'en-US',
+};
+
+export { SITE };
