637fdd1f9b503f57390e0a73a345526c296067f4
Jeroen Druwe <jeroen.druwe@weareida.digital>
2026-01-05 18:26:29 +0100

    updated intro

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 const SITE = {
   title: 'Jeroen Druwé',
   description:
-    'Software developer and tech enthusiast. In my spare time, I’m tinkering with smart home tech, playing the piano, geocaching, or spending time with family.',
+    'Software developer and tech enthusiast. In my spare time, I’m tinkering with smart home tech, playing the piano, cycling, geocaching, or spending time with family.',
   href: 'https://jeroendruwe.be',
   author: 'Jeroen Druwé',
   locale: 'en-US',
