30ed621aab5c5fb3acfa0d2a9969231e0ab0d915
Jeroen Druwe <jeroen.druwe@weareida.digital>
2026-03-27 16:10:34 +0100

    upgrading version

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs));
+};
+
+const formatDateLong = (date: Date) => {
+  return Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }).format(date);
+};
+
+const formatDateISO = (date: Date) => {
+  return date.toISOString().split('T')[0];
+};
+
+export { cn, formatDateLong, formatDateISO };
